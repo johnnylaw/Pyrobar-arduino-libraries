d00c259ff98ac192bad8ae480ca5f557d3760af2
John Lawrence <johnonrails@gmail.com>
2014-07-19 12:28:43 -0700

    Add PyrobarConstants.h

---
+++ PyrobarConstants.h
@@ -0,0 +1,9 @@
+#ifndef _PYROBAR_CONSTANTS_H
+#define _PYROBAR_CONSTANTS_H
+
+#define _DEBUG true
+#define DEBUG_REQUEST_HANDLER false
+#define DEBUG_LIGHT_MAP false
+#define TOTAL_ZONE_COUNT 9
+
+#endif
