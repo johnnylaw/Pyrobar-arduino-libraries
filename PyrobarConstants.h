64389bdbe21bc2f7f1264727af96ad0b716a5a0e
John Lawrence <johnonrails@gmail.com>
2014-07-22 21:18:24 -0700

    Define UDP constants and OFF and ON

--- PyrobarConstants.h
+++ PyrobarConstants.h
@@ -10,4 +10,12 @@
 #define TOTAL_ZONE_COUNT 9
 #define COLOR_COUNT 3
 
+#define UDP_FIRE_ON 0
+#define UDP_FIRE_OFF 1
+#define UDP_PULSE_LIGHT_ON 2
+#define UDP_PULSE_LIGHTS_DECAY_ON 3
+
+#define ON true
+#define OFF false
+
 #endif
