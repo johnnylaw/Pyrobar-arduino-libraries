c59ac06a99a329fdb2c584e1dd8668b2a6e31bf6
John Lawrence <johnonrails@gmail.com>
2014-07-26 17:12:35 -0700

    Clean up sketch

--- PyrobarMaster.ino
+++ PyrobarMaster.ino
@@ -1,37 +1,42 @@
 #include <SPI.h>
 #include <Ethernet.h>
-//#include <EthernetUdp.h>
+#include <EthernetUdp.h>
 #include <Wire.h>
 #include "PyrobarConstants.h"
 #include "PyrobarFireCannon.h"
 #include "PyrobarLightMaster.h"
-#include "PyrobarRequestHandler.h"
+#include "PyrobarHTTPRequestHandler.h"
+#include "PyrobarUDPRequestHandler.h"
 #include "PyrobarFireController.h"
+#include "PyrobarPulseLightSet.h"
 
 #define UDP_FIRE_ON 0
 #define UDP_FIRE_OFF 1
 #define UDP_PULSE_LIGHT_ON 2
 #define UDP_PULSE_LIGHTS_DECAY_ON 3
+#define NUM_FIRE_CANNONS 3
 
-byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+uint8_t mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 IPAddress ip(192, 168, 1, 101);
 unsigned int localPortUDP = 8888;
 
 EthernetUDP Udp;
 
-#define UDP_TX_PACKET_MAX_SIZE 12
 unsigned char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
 
-uint8_t firePins[] = {25, 26, 27};
+uint8_t firePins[NUM_FIRE_CANNONS] = {25, 26, 27};
 uint8_t ledPins[] = {2, 3, 4, 5, 6, 7};
 
-PyrobarFireCannon fireCannons[] = {PyrobarFireCannon(firePins[0]), PyrobarFireCannon(firePins[1]), PyrobarFireCannon(firePins[2])};
-const int NUM_FIRE_CANNONS = sizeof(fireCannons) / sizeof(PyrobarFireCannon);
+PyrobarFireCannon fireCannons[NUM_FIRE_CANNONS] = {PyrobarFireCannon(firePins[0]), PyrobarFireCannon(firePins[1]), PyrobarFireCannon(firePins[2])};
 static PyrobarLightValueMap lightMap = PyrobarLightValueMap();
-static PyrobarLightMaster MasterCtrl = PyrobarLightMaster(lightMap, ledPins);
-static PyrobarRequestHandler PBRequestHandler = PyrobarRequestHandler(&lightMap);
-static PyrobarFireSequence fireSequence = PyrobarFireSequence();
-static PyrobarFireController FireCtrl = PyrobarFireController(NUM_FIRE_CANNONS, firePins, fireSequence);
+static PyrobarPulseLightSet pulseLightSet = PyrobarPulseLightSet();
+
+static PyrobarLightMaster MasterCtrl = PyrobarLightMaster(&lightMap, &pulseLightSet, ledPins);
+static PyrobarHTTPRequestHandler PBHTTPRequestHandler = PyrobarHTTPRequestHandler(&lightMap);
+static PyrobarUDPRequestHandler PBUDPRequestHandler = PyrobarUDPRequestHandler(&lightMap, &pulseLightSet);
+
+//static PyrobarFireSequence fireSequence = PyrobarFireSequence();
+//static PyrobarFireController FireCtrl = PyrobarFireController(NUM_FIRE_CANNONS, firePins, fireSequence);
 
 uint8_t freqBfrPos;
 uint8_t sndBfrPos;
@@ -41,44 +46,40 @@ EthernetServer server(80);
 void setup() {
   Serial.begin(9600);
   Ethernet.begin(mac, ip);
-  FireCtrl.begin();
   Udp.begin(localPortUDP);
+  MasterCtrl.begin();
+//  FireCtrl.begin();
   Serial.println(Ethernet.localIP());
   pinMode(38, INPUT);
-  
-  if (DEBUG_LIGHT_MAP) {
-    Serial.print("Light map in main at address ");
-    Serial.println((long)&lightMap);
-  }
 }
 
 void loop() {
-  if(!FireCtrl.play()) {
-    if (int packetSize = Udp.parsePacket()) {
-      if (DEBUG_UDP) {
-        Serial.print("UDP packet received with packet size: ");
-        Serial.println(packetSize);
-      }
-      Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
-      if (DEBUG_UDP) {
-        for (int i = 0; i < packetSize; i++) Serial.println(packetBuffer[i]);
-      }
-      // parse packetBuffer info for cannon #, start/kill bit and duration if start
+  if (int packetSize = Udp.parsePacket()) {
+    if (DEBUG_UDP) {
+      Serial.print("UDP packet received with packet size: ");
+      Serial.println(packetSize);
     }
-    if(digitalRead(38) && _DEBUG) {
-      printDiagnostics();
-    }
-    if (EthernetClient client = server.available()) {
-      killCannons();
-      if (_DEBUG) Serial.println("\nClient exists!");
-      PBRequestHandler.handleHTTPRequest(client);
-    } else {
-      adjustCannons();
-    }
+    Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+    PBUDPRequestHandler.handleRequest(packetBuffer, packetSize);
+  }
+  if(digitalRead(38) && _DEBUG) {
+    printDiagnostics();
+  }
+  Wire.beginTransmission(0x10);
+  for (int i = 0; i < 12; i++) {
+    Wire.write(0);
+  }
+  Wire.endTransmission();
+  if (EthernetClient client = server.available()) {
+    killCannons();
+    if (_DEBUG) Serial.println("\nClient exists!");
+    PBHTTPRequestHandler.handleRequest(client);
+  } else {
+    adjustCannons();
   }
 
-//  MasterCtrl.calculateBufferPositions(&freqBfrPos, &sndBfrPos);
-//  MasterCtrl.sendLightValues(freqBfrPos, sndBfrPos);
+  MasterCtrl.calculateBufferPositions(&freqBfrPos, &sndBfrPos);
+//  MasterCtrl.sendLightProgramInfo(freqBfrPos, sndBfrPos);
   delay(50);
 }
 
