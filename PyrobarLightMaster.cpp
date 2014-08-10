b9b1899add9186d208e81d1c6977b6ab3237c9bc
John Lawrence <johnonrails@gmail.com>
2014-08-10 01:44:49 -0700

    Write all zones at once instead of knowing about boards

--- PyrobarLightMaster.cpp
+++ PyrobarLightMaster.cpp
@@ -3,60 +3,40 @@
 #include "PyrobarLightMaster.h"
 #include "PololuLedStrip.h"
 
-PyrobarLightMaster::PyrobarLightMaster(PyrobarLightMap *lightMap, PyrobarPulseLightSet *pulseLightSet, uint8_t *ledPins, uint8_t soundLevelPin) : _lastCyclePosition(0.0), _lightMap(lightMap), _pulseLightSet(pulseLightSet), _ledPins(ledPins), _soundLevelPin(soundLevelPin) {
+PyrobarLightMaster::PyrobarLightMaster(PyrobarLightMap *lightMap, PyrobarPulseLightSet *pulseLightSet, uint8_t *ledPins, uint8_t soundLevelPin) : _lastCyclePosition(0.0), _lightMap(lightMap), _pulseLightSet(pulseLightSet), _soundLevelPin(soundLevelPin) {
   _numberOfSlaves = (TOTAL_ZONE_COUNT - 1) / ZONES_PER_SLAVE_BOARD;
 }
 
 void PyrobarLightMaster::sendLightProgramInfo(uint8_t freqBfrPos, uint8_t sndBfrPos) {
-  // go thru slave boards, write ZONES_PER_SLAVE_BOARD * COLOR_COUNT values to Wire for each
   uint8_t value;
-  // Write zones on slave boards
-  for (unsigned char board = 0; board < _numberOfSlaves; board++) {
-    Wire.beginTransmission(BASE_I2C_ADDRESS + board);
-    if (DEBUG_LIGHT_OUTPUT) {
-      Serial.print("Transmitting to ");
-      Serial.println(BASE_I2C_ADDRESS + board);
-    }
-    for (unsigned char boardZone = 0; boardZone < ZONES_PER_SLAVE_BOARD; boardZone++) {
-      unsigned char zoneIndex = board * ZONES_PER_SLAVE_BOARD + boardZone;
-      for (unsigned char color = 0; color < COLOR_COUNT; color++) {
-        if (_lightMap->shouldDisplay()) {
-          uint8_t freqValue = _lightMap->read(pyrobarBfrTypeFreq, zoneIndex, freqBfrPos, color);
-          uint8_t sndValue = _lightMap->read(pyrobarBfrTypeSnd, zoneIndex, sndBfrPos, color);
-          value = max(freqValue, sndValue);
-        } else {
-          value = _pulseLightSet->read(zoneIndex, color);
-        }
-        if (DEBUG_LIGHT_OUTPUT && boardZone == 0) {
-          Serial.println(value);
-        }
-        Wire.write(value);
-      }
-    }
-    Wire.endTransmission();
+  Wire.beginTransmission(BASE_I2C_ADDRESS);
+
+  if (DEBUG_LIGHT_OUTPUT) {
+    Serial.print("Transmitting to ");
+    Serial.println(BASE_I2C_ADDRESS);
   }
 
-  // Write zones that are on master board
-  for (int zone = ZONES_PER_SLAVE_BOARD * _numberOfSlaves; zone < TOTAL_ZONE_COUNT; zone++) {
-    int localZoneIndex = zone % ZONES_PER_SLAVE_BOARD;
-    for (int color = 0; color < COLOR_COUNT; color++) {
+  for (int zoneInd = 0; zoneInd < TOTAL_ZONE_COUNT; zoneInd++) {
+    for (int colorInd = 0; colorInd < COLOR_COUNT; colorInd++) {
       if (_lightMap->shouldDisplay()) {
-        uint8_t freqValue = _lightMap->read(pyrobarBfrTypeFreq, zone, freqBfrPos, color);
-        uint8_t sndValue = _lightMap->read(pyrobarBfrTypeSnd, zone, sndBfrPos, color);
+        uint8_t freqValue = _lightMap->read(pyrobarBfrTypeFreq, zoneInd, freqBfrPos, colorInd);
+        uint8_t sndValue = _lightMap->read(pyrobarBfrTypeSnd, zoneInd, sndBfrPos, colorInd);
         value = max(freqValue, sndValue);
       } else {
-        value = _pulseLightSet->read(zone, color);
+        value = _pulseLightSet->read(zoneInd, colorInd);
       }
-      analogWrite(_ledPins[localZoneIndex * COLOR_COUNT + color], value);
+      if (DEBUG_LIGHT_OUTPUT && zoneInd == 0) {
+        Serial.println(value);
+      }
+      Wire.write(value);
     }
   }
+
+  Wire.endTransmission();
 }
 
 void PyrobarLightMaster::begin(void) {
   Wire.begin();
-  for (int i = 0; i < sizeof(_ledPins); i++) {
-    pinMode(_ledPins[i], OUTPUT);
-  }
   pinMode(_soundLevelPin, INPUT);
 }
 
